bf3964b29f0d33d46cd43c54846f70a61e69d2d7
Dan van den Berg <dan@shopkeep.com>
2013-11-10 16:15:47 -0500

    Versioning with patch number

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "paul.guelpa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures lockrun"
 
-version          "0.0.2"
+version          "0.2.0"
 
 depends "build-essential"
