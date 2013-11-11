adb184737e07dc5cdf10360a497fa4f6f7fbea49
Paul Guelpa <paul.guelpa@gmail.com>
2013-11-11 15:55:36 -0500

    Update meta-data to match release information

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "ShopKeep"
-maintainer_email "paul@shopkeep.com"
+maintainer       "Paul Guelpa"
+maintainer_email "paul.guelpa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures lockrun"
 
-version          "0.2"
+version          "0.0.2"
 
 depends "build-essential"
