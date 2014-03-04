47dda8605e6f4a2e84749f630701d6eea8ebb9a3
Dan van den Berg <dan@shopkeep.com>
2014-03-04 10:30:31 -0500

    Decided to do only a patch version change

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "paul.guelpa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures lockrun"
 
-version          "0.2.0"
+version          "0.0.3"
 
 depends "build-essential"
